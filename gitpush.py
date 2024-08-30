3a0cd5898b1834a9e08d4eaa238f8cd78354668c
Ravi Kant <ravik@Ravis-MacBook-Air.local>
2024-08-31 00:20:52 +0530

    updated script 2

---
+++ gitpush.py
@@ -0,0 +1,25 @@
+#!/opt/homebrew/bin/python3
+import subprocess
+import sys
+
+def automate_git_commands(commit_message):
+    try:
+        subprocess.run(["git", "add", "."], check=True)
+        print("Files added to staging area.")
+
+        subprocess.run(["git", "commit", "-m", commit_message], check=True)
+        print(f"Committed with message: '{commit_message}'")
+
+        subprocess.run(["git", "push", "origin", "main", "-f"], check=True)
+        print("Pushed changes to origin main with force.")
+
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: git-automate 'commit message'")
+        sys.exit(1)
+    
+    commit_message = sys.argv[1]
+    automate_git_commands(commit_message)
